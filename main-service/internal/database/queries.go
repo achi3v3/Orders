0dba3826cbd92bc634eacecd8b4e47546020742a
achi3v3 <amir-tutaev@mail.ru>
2025-09-06 20:28:18 +0500

    feat(back): update queries structure

---
+++ main-service/internal/database/queries.go
@@ -0,0 +1,70 @@
+package database
+
+type TableCreate interface {
+	createOrders() string
+	createDeliveries() string
+	createPayments() string
+	createItems() string
+}
+type TableCreator struct{}
+
+func NewTableCreator() TableCreate {
+	return &TableCreator{}
+}
+func (c *TableCreator) createOrders() string {
+	return `CREATE TABLE IF NOT EXISTS orders (
+			order_uid VARCHAR(255) PRIMARY KEY NOT NULL,
+			track_number VARCHAR(255) NOT NULL UNIQUE,
+			entry VARCHAR(20) NOT NULL,
+			locale VARCHAR(10) NOT NULL,
+			internal_signature VARCHAR(255) DEFAULT '',
+			customer_id VARCHAR(255) NOT NULL,
+			delivery_service VARCHAR(100) NOT NULL,
+			shardkey VARCHAR(20) NOT NULL,
+			sm_id INTEGER NOT NULL,
+			date_created TIMESTAMPTZ NOT NULL,
+			oof_shard VARCHAR(20) NOT NULL
+	);`
+}
+func (c *TableCreator) createDeliveries() string {
+	return `CREATE TABLE IF NOT EXISTS deliveries (
+			order_uid VARCHAR(255) PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
+			name VARCHAR(255) NOT NULL,
+			phone VARCHAR(50) NOT NULL,
+			zip VARCHAR(50) NOT NULL,
+			city VARCHAR(255) NOT NULL,
+			address TEXT NOT NULL,
+			region VARCHAR(255) NOT NULL,
+			email VARCHAR(255) NOT NULL
+	);`
+}
+func (c *TableCreator) createPayments() string {
+	return `CREATE TABLE IF NOT EXISTS payments (
+			transaction VARCHAR(255) PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
+			request_id VARCHAR(255) DEFAULT '',
+			currency VARCHAR(20) NOT NULL,
+			provider VARCHAR(150) NOT NULL,
+			amount INTEGER NOT NULL,
+			payment_dt BIGINT NOT NULL,
+			bank VARCHAR(150) NOT NULL,
+			delivery_cost INTEGER NOT NULL,
+			goods_total INTEGER NOT NULL,
+			custom_fee INTEGER DEFAULT 0
+	);`
+}
+func (c *TableCreator) createItems() string {
+	return `CREATE TABLE IF NOT EXISTS items (
+			id SERIAL PRIMARY KEY,
+			chrt_id BIGINT NOT NULL,
+			track_number VARCHAR(255) REFERENCES orders(track_number) ON DELETE CASCADE,
+			price INTEGER NOT NULL,
+			rid VARCHAR(255) NOT NULL,
+			name VARCHAR(255) NOT NULL,
+			sale INTEGER NOT NULL,
+			size VARCHAR(20) DEFAULT '0',
+			total_price INTEGER NOT NULL,
+			nm_id INTEGER NOT NULL,
+			brand VARCHAR(255) NOT NULL,
+			status INTEGER NOT NULL
+	);`
+}
--- main-service/internal/database/tables.go
+++ main-service/internal/database/tables.go
@@ -5,70 +5,41 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/sirupsen/logrus"
 )
 
-func CreateTables(ctx context.Context, conn *pgx.Conn) error {
-	tx, err := conn.Begin(ctx)
+type HandlerDB struct {
+	conn   *pgx.Conn
+	logger *logrus.Logger
+}
+
+func NewHandlerDB(conn *pgx.Conn, logger *logrus.Logger) *HandlerDB {
+	return &HandlerDB{
+		conn:   conn,
+		logger: logger,
+	}
+}
+
+func (h *HandlerDB) CreateTables(ctx context.Context, conn *pgx.Conn) error {
+	tx, err := h.conn.Begin(ctx)
 	if err != nil {
-		return fmt.Errorf("subs.CreateTables: %w", err)
+		return fmt.Errorf("HandlerDB.CreateTables: %w", err)
 	}
 	defer tx.Rollback(ctx)
 
+	creator := NewTableCreator()
+
 	queries := []string{
-		`CREATE TABLE IF NOT EXISTS orders (
-			order_uid VARCHAR(255) PRIMARY KEY NOT NULL,
-			track_number VARCHAR(255) NOT NULL UNIQUE,
-			entry VARCHAR(20) NOT NULL,
-			locale VARCHAR(10) NOT NULL,
-			internal_signature VARCHAR(255) DEFAULT '',
-			customer_id VARCHAR(255) NOT NULL,
-			delivery_service VARCHAR(100) NOT NULL,
-			shardkey VARCHAR(20) NOT NULL,
-			sm_id INTEGER NOT NULL,
-			date_created TIMESTAMPTZ NOT NULL,
-			oof_shard VARCHAR(20) NOT NULL
-	);`,
-		`CREATE TABLE IF NOT EXISTS deliveries (
-			order_uid VARCHAR(255) PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
-			name VARCHAR(255) NOT NULL,
-			phone VARCHAR(50) NOT NULL,
-			zip VARCHAR(50) NOT NULL,
-			city VARCHAR(255) NOT NULL,
-			address TEXT NOT NULL,
-			region VARCHAR(255) NOT NULL,
-			email VARCHAR(255) NOT NULL
-	);`,
-		`CREATE TABLE IF NOT EXISTS payments (
-			transaction VARCHAR(255) PRIMARY KEY REFERENCES orders(order_uid) ON DELETE CASCADE,
-			request_id VARCHAR(255) DEFAULT '',
-			currency VARCHAR(20) NOT NULL,
-			provider VARCHAR(150) NOT NULL,
-			amount INTEGER NOT NULL,
-			payment_dt BIGINT NOT NULL,
-			bank VARCHAR(150) NOT NULL,
-			delivery_cost INTEGER NOT NULL,
-			goods_total INTEGER NOT NULL,
-			custom_fee INTEGER DEFAULT 0
-	);`,
-		`CREATE TABLE IF NOT EXISTS items (
-			id SERIAL PRIMARY KEY,
-			chrt_id BIGINT NOT NULL,
-			track_number VARCHAR(255) REFERENCES orders(track_number) ON DELETE CASCADE,
-			price INTEGER NOT NULL,
-			rid VARCHAR(255) NOT NULL,
-			name VARCHAR(255) NOT NULL,
-			sale INTEGER NOT NULL,
-			size VARCHAR(20) DEFAULT '0',
-			total_price INTEGER NOT NULL,
-			nm_id INTEGER NOT NULL,
-			brand VARCHAR(255) NOT NULL,
-			status INTEGER NOT NULL
-	);`,
+		creator.createOrders(),
+		creator.createDeliveries(),
+		creator.createPayments(),
+		creator.createItems(),
 	}
+
 	for _, query := range queries {
 		_, err := tx.Exec(ctx, query)
 		if err != nil {
-			return fmt.Errorf("%w", err)
+			return fmt.Errorf("HandlerDB.CreateTables: %w", err)
 		}
 	}
 
